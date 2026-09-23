6f0b4b1ed06451faa69a5e2196866ad63eeb3013
joao-cabral <38953911+joao-cabral@users.noreply.github.com>
2026-09-23 12:56:09 -0400

    feat: add Casa Segura static site

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL("./index.html", import.meta.url)),
+        casaSegura: fileURLToPath(
+          new URL("./casa-segura/index.html", import.meta.url)
+        ),
+      },
+    },
+  },
 });
